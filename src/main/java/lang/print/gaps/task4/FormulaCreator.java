b873a68dcf059ec9dcca1c79d861a1e7f77d2474
rau1310 <rmusakhan@college-narxoz.kz>
2022-07-27 14:58:01 +0600

    gl

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+
+        System.out.println((9*a*a-5*b+2+a-7)*((a+2-4*a*b)/2));
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char charC = 'c';
+        char charH = 'h';
+        char charA = 'a';
+        char charR = 'r';
+        System.out.println((int)charC);
+        System.out.println((int)charH);
+        System.out.println((int)charA);
+        System.out.println((int)charR);
     }
 }
